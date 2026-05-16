6ae1afb90d833dad0d05d4e4bb4743cbf9af90e7
marcosdima <marcosdimatteo.wgr@gmail.com>
2026-05-16 12:10:18 -0300

    font_sizes

--- game/src/autoload/font.gd
+++ game/src/autoload/font.gd
@@ -2,3 +2,8 @@
 extends Node
 
 var font: Font = load("res://assets/fonts/JetBrainsMono-2.304/fonts/ttf/JetBrainsMono-Regular.ttf")
+
+const SMALL = 12
+const MEDIUM = 16
+const LARGE = 24
+const XLARGE = 32
